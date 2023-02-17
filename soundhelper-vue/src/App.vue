5b82d71904f4bf00e4c950ed13e5fd19f6fa64b0
jiminpark01 <bssjm17@gmail.com>
2023-02-17 23:00:31 +0900

    첫화면 뼈대

--- soundhelper-vue/src/App.vue
+++ soundhelper-vue/src/App.vue
@@ -1,7 +1,25 @@
 <template>
-
+    <div>
+        <home-view></home-view>
+    </div>
 </template>
+  
+<script>
+import HomeView from './views/HomeView.vue'
 
+export default {
+    components: {HomeView},
+    data() {
+        return {}
+    },
+
+}
+</script>
+  
+  
 <style>
-
+div {
+    width: 320px;
+    height: 568px;
+}
 </style>
--- soundhelper-vue/src/views/HomeView.vue
+++ soundhelper-vue/src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+  <div id="menu">
+    <button id="sound_mode">Sound Mode</button>
     <stock-list></stock-list>
   </div>
 </template>
@@ -18,11 +19,30 @@ export default {
 
 
 <style>
-div {
+#menu {
   width: 320px;
-  height: 500px;
-  background-image: url('C:\Users\user\Desktop\Sound-Helper\soundhelper-vue\public\menu.JPG');
+  height: 568px;
+  background-image: url('C:\Users\jimin\대학교\3학년\겨울방학\신한투자증권 프로 디지털 아카데미\최종프로젝트\soundhelper-vue\public\menu.png');
   margin: auto;
   display: block;
+}
+
+#sound_mode {
+  position: absolute;
+  width: 65px;
+  height: 65px;
+  left: 13px;
+  top: 460px;
+  background-color: #41FFCF;
+  border-radius: 12px;
+  font-family: 'IBM Plex Sans';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 26px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  color: #000000;
 }
 </style>
