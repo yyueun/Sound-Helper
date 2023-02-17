bb5ed79f5fa700cc35b40f956dc2795329c4d05e
“jiminpark23” <jimin4124.22@gmail.com>
2023-02-17 17:49:52 +0900

    파일 옮김

--- soundhelper-vue/src/App.vue
+++ soundhelper-vue/src/App.vue
@@ -1,35 +1,7 @@
 <template>
-  <div>
-    <stock-list></stock-list>
-  </div>
+
 </template>
 
-<script>
-import StockList from './components/StockList.vue'
-
-export default {
-  components: {StockList},
-  data() {
-    return {}
-  },
-  
-}
-</script>
-
-
 <style>
-div {
-  width: 320px;
-  height: 500px;
-  background-image: url('C:\Users\user\Desktop\Sound-Helper\soundhelper-vue\public\menu.JPG');
-  margin: auto;
-  display: block;
-}
-div > button {
-  width: 15px;
-  height: 30px;
-}
+
 </style>
-
-
-:style="{'background-image':'url(soundhelper-vue/public/menu.JPG)'}
--- soundhelper-vue/src/views/HomeView.vue
+++ soundhelper-vue/src/views/HomeView.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <stock-list></stock-list>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import StockList from './StockList.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  components: {StockList},
+  data() {
+    return {}
+  },
+  
 }
 </script>
+
+
+<style>
+div {
+  width: 320px;
+  height: 500px;
+  background-image: url('C:\Users\user\Desktop\Sound-Helper\soundhelper-vue\public\menu.JPG');
+  margin: auto;
+  display: block;
+}
+</style>
