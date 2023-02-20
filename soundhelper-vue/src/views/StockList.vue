2e8b6cf07abc3fe62b7c7cb0f98684745cec20e1
“jiminpark23” <jimin4124.22@gmail.com>
2023-02-20 17:49:02 +0900

    주문페이지

--- soundhelper-vue/src/views/StockList.vue
+++ soundhelper-vue/src/views/StockList.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="page">
     <div>
+      <img src="../assets/top-bar.png" style="height:32px; width: 320px">
+    </div>
+    <div id="top">
       <!-- 상단 바 (뒤로가기, 실시간종목순위, 하트, 더보기) -->
+      <button style="width:32px; height: 33px; float:left; border: none;">&lt</button>
+      <span id="rank">실시간 종목순위</span>
+      <span id="more">.&nbsp;.&nbsp;.<br>더보기</span>
+      <i class="fa-regular fa-heart" id="heart-icon"></i>
     </div>
     <br>
     <div id="search-container">
       <!-- 검색창 -->
-      <input id="search" type="text" placeholder="검색" v-model="searchTerm" @input="search">
+      <input id="search" type="text" placeholder="&nbsp&nbsp검색" v-model="searchTerm" @input="search">
     </div>
     <br>
     <div>
       <!-- 정렬 버튼  -->
-      <!-- 내비게이션 바로 다시 수정하기 -->
       <ul class="nav nav-tabs justify-content-center">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" @click="sortByMarketCap">시가총액순</a>
@@ -20,19 +26,25 @@
           <a class="nav-link disabled" @click="sortByVolume">거래량순</a>
         </li>
       </ul>
-      <!-- <button @click="sortByMarketCap">시가총액순</button>
-      <button @click="sortByVolume">거래량순</button> -->
     </div>
-    <br>
-    <div>
+    <div style="height: 270px; overflow: scroll;">
       <!-- 주식 종목 목록 -->
       <div v-for="stock in stocks" :key="stock.id">
-        <h3>{{ stock.name }} {{ stock.price }} {{ stock.fluctuationRate }}%</h3>
+        <table>
+            <tbody>
+                <tr>
+                    <td style="width:150px; text-align:left; padding: 10px">{{ stock.name }}</td>
+                    <td style="width: 100px; text-align: right;">{{ stock.price }}</td>
+                    <td  v-if="stock.fluctuationRate < 0" style="width: 70px; text-align: right; padding: 10px; color: blue">{{ (-1) * Number(stock.fluctuationRate) }}%</td>
+                    <td  v-if="stock.fluctuationRate > 0" style="width: 70px; text-align: right; padding: 10px; color: red">{{ stock.fluctuationRate }}%</td>
+                </tr>
+            </tbody>
+        </table>
         <hr>
       </div>
     </div>
     <div>
-      <img src="soundhelper-vue\public\bottomBar.png">
+      <img src="../assets/bottomBar.png" id="bottom-bar">
     </div>
     <router-view></router-view>
   </div>
@@ -44,11 +56,14 @@ export default {
     return {
       searchTerm: '',
       stocks: [
-        { id: 1, name: '삼성전자', price: 62000, fluctuationRate: -1.58, marketCap: 520000000000, volume: 1000000 },
-        { id: 2, name: 'SK하이닉스', price: 91700, fluctuationRate: 0.38, marketCap: 80000000000, volume: 500000 },
-        { id: 3, name: 'LG화학', price: 800000, fluctuationRate: -1.82, marketCap: 60000000000, volume: 200000 },
-        { id: 4, name: 'NAVER', price: 450000, fluctuationRate: 7.26, marketCap: 450000000000, volume: 300000 },
-      ],
+          { id: 1, name: '삼성전자', price: 62000, fluctuationRate: -1.58, marketCap: 520000000000, volume: 1000000 },
+          { id: 2, name: 'LG에너지솔루션', price: 516000, fluctuationRate: -3.19, marketCap: 80000000000, volume: 500000 },
+          { id: 3, name: 'SK하이닉스', price: 92500, fluctuationRate: 0.54, marketCap: 60000000000, volume: 200000 },
+          { id: 4, name: '삼성바이오로직스', price: 805000, fluctuationRate: 0.75, marketCap: 450000000000, volume: 300000 },
+          { id: 5, name: '삼성SDI', price: 692000, fluctuationRate: -1.56, marketCap: 450000000000, volume: 300000 },
+          { id: 6, name: 'LG화학', price: 667000, fluctuationRate: -3.19, marketCap: 450000000000, volume: 300000 },
+          { id: 7, name: '삼성전자우', price: 55900, fluctuationRate: 0.18, marketCap: 450000000000, volume: 300000 },
+        ],
       sortOption: 'marketCap',
       sortAscending: false,
     };
@@ -89,6 +104,37 @@ export default {
 #page {
   width: 320px;
   height: 568px;
+  border: 1px solid;
+}
+
+#top {
+  position: fixed;
+  width: 320px;
+  height: 33px;
+  background-color: lightgray;
+}
+
+#rank {
+  line-height: 33px;
+  float: left;
+  margin-left: 10px;
+}
+
+#more {
+  height: 33px;
+  width: 33px;
+  float: right;
+  font-size: 10px;
+  vertical-align: middle;
+  margin-top: 3px;
+}
+
+#heart-icon {
+  height: 33px;
+  width: 25px;
+  align-items: center;
+  margin-top: 9px;
+  float:right;
 }
 
 #search-container {
@@ -103,4 +149,8 @@ export default {
   margin: 0 auto;
 }
 
+#bottom_bar {
+  position: fixed;
+  width: 320px;
+}
 </style>
--- soundhelper-vue/src/views/StockOrder.vue
+++ soundhelper-vue/src/views/StockOrder.vue
@@ -1,49 +1,71 @@
 <template>
-    <div>
+    <div id="page">
         <div>
-            <!-- 상단 바 -->
-            <!-- 음표버튼 -->
+            <img src="../assets/top-bar.png" style="height:32px; width: 320px">
+        </div>
+        <div>
+            <!-- 상단 바, 음표버튼 -->
+            <button style="width:32px; height: 33px; float:left; border: none;">&lt</button>
+            <span id="name" style="width: 150px;">신한지주</span>
+            <i class="fa-solid fa-magnifying-glass" id="search-icon"></i>
+            <i class="fa-solid fa-music" id="music-icon"></i>
         </div>
         <div id="chart-container">
             <!-- 차트 -->
             <div id="chart"></div>
             <!-- 현재가 -->
-            <h4 id="current-price">38,300</h4>
+            <h2 id="current-price">38,300</h2>
             <!-- 일반모드 전환 -->  
             <button id="normal-mode">일반</button>
         </div>
-        <div>
+        <div id="count">
             <!-- 플러스마이너스 버튼, 현재 수 -->
+            <button :style="{ 'background-color': '#FB5A6B' }" @click="decrement">-1</button>
+            <input type="text" v-model="count">
+            <button :style="{ 'background-color': '#6F4BFD' }" @click="increment">+1</button>
         </div>
-        <div>
+        <div id="btn-set">
             <!-- 구매 판매 버튼 -->
+            <button id="button" :style="{ 'background-color': '#FB5A6B'}">구매</button>
+            <button id="button" :style="{ 'background-color': '#6F4BFD'}">판매</button>
         </div>
-        <div>
+        <div id="voice">
             <!-- 음성인식 -->
+            <button id="button">음성인식</button>
         </div>
-        <div>
+        <div id="btn-set">
             <!-- 예수금, 수익률 버튼 -->
+            <button id="button">예수금</button>
+            <button id="button">수익률</button>
         </div>
-        <div>
-            <img src="soundhelper-vue\public\bottomBar.png">
+        <div id="bar">
+            <img src="..\assets\bottomBar.png">
         </div>
     </div>
 </template>
 
 <script>
 export default {
-
+    
 }
+
 </script>
 
 <style>
+#page {
+  width: 320px;
+  height: 568px;
+  border: 1px solid;
+}
+
 #chart-container {
     position: relative;
+    border: 1px solid;
 }
 
 #chart {
     width: 300px;
-    height: 227px;
+    height: 230px;
     top: 30px;
     left: 10px;
 }
@@ -54,6 +76,8 @@ export default {
     position: absolute;
     top: 7px;
     left: 7px;
+    font-weight: bold;
+    color: blue;
 }
 
 #normal-mode {
@@ -74,4 +98,63 @@ export default {
     border-radius: 12px;
 }
 
+#name {
+    line-height: 33px;
+    float: left;
+    margin-left: 10px;
+    border: 1px solid;
+}
+
+#search-icon {
+    width: 25px;
+    height: 33px;
+    align-items: center;
+    margin-top: 10px;
+    margin-left: 10px;
+}
+
+#music-icon {
+    margin-left: 60px;
+}
+
+#count {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+  margin-top: 5px;
+}
+
+#btn-set {
+    display: flex;
+    justify-content: center;
+}
+
+#voice {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+}
+
+#button {
+  justify-content: space-between;
+  width: 130px;
+  height: 45px;
+  border: 1px solid;
+  border-radius: 25px;
+  padding: 10px;
+  margin: 5px 10px 5px 10px;
+  background-color: rgb(241, 241, 241);
+}
+
+#bar {
+  position: relative;
+  top: 2px;
+}
+
+#bar img {
+    position: absolute;
+    width: 320px;
+}
 </style>
